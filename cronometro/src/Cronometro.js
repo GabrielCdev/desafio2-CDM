916cd78b4a7d46f732eeac82de6e97e57477ec07
GuidoGomes <guidoggj@hotmail.com>
2020-10-12 19:45:35 -0300

    Atualizações para o Cronômetro

--- cronometro/src/Cronometro.js
+++ cronometro/src/Cronometro.js
@@ -4,7 +4,7 @@ import BtnComponent from './Components/BtnComponent';
 import './App.css';
 
 function App() {
-  const [time, setTime] = useState({cs:0, ms:0, s:0, m:0, h:0});
+  const [time, setTime] = useState({cs:0, s:0, m:0, h:0});
   const [interv, setInterv] = useState();
   const [status, setStatus] = useState(0);
   // Não iniciado = 0
@@ -19,7 +19,7 @@ function App() {
     setInterv(setInterval(run, 1)); // Verificar esse valor
   };
 
-  var atualizaCs = time.cs, atualizaMs = time.ms, atualizaS = time.s, atualizaM = time.m, atualizaH = time.h;
+  var atualizaCs = time.cs, atualizaS = time.s, atualizaM = time.m, atualizaH = time.h;
 
   const run = () => {
     if(atualizaM === 60) {
@@ -32,18 +32,13 @@ function App() {
       atualizaS = 0;
     }
 
-    if(atualizaMs === 100) {
+    if(atualizaCs === 100) { 
       atualizaS++;
-      atualizaMs = 0;
-    }
-
-    if(atualizaCs === 100) { // Verificar esse valor, que corresponde aos centésimos de segundo!!!
-      atualizaMs++;
       atualizaCs = 0;
     }
 
     atualizaCs++;
-    return setTime({cs:atualizaCs, ms:atualizaMs, s:atualizaS, m:atualizaM, h:atualizaH});
+    return setTime({cs:atualizaCs, s:atualizaS, m:atualizaM, h:atualizaH});
   };
 
   const parar = () => {
@@ -54,15 +49,18 @@ function App() {
   const zerar = () => {
     clearInterval(interv);
     setStatus(0);
-    setTime({cs:0, ms:0, s:0, m:0, h:0});
+    setTime({cs:0, s:0, m:0, h:0});
   };
 
   const retomar = () => iniciar();
 
   const tempoParcial = () => { 
     setStatus(3);
+    let time = this.state.H+ ":"+ this.state.M+ ":"+ this.state.S+ ":"+ this.state.Cs+ "\n\n"
     setParcial([...parcial, time]);
   };
+
+  tempoParcial();
 
   return (
     <div className="App-header">
