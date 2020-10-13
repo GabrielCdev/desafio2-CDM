02d3e904d11974c811bfdf22deeee7394d97ebed
Gabriel <gabi_borges01@hotmail.com>
2020-10-12 22:49:49 -0300

    Colocando o cronometro como App.js

--- cronometro/src/App.js
+++ cronometro/src/App.js
@@ -1,68 +1,77 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import DisplayComponent from './Components/DisplayComponent';
+import BtnComponent from './Components/BtnComponent';
 import './App.css';
-import Clock from './Clock';
 
-const clocks = [];
-const num = 1;
-for(let i = 0; i < num; i++) {
-    clocks.push({
-        timezone: "America/Bahia",
-        name: "Salvador",
-        id: i,
-        done: false
-    })
-}
+function App() {
+  const [time, setTime] = useState({cs:0, s:0, m:0, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  // Não iniciado = 0
+  // Iniciado = 1
+  // Parado = 2
+  const [parcial, setParcial] = useState(3);
+  // Parcial = 3
+  
+  const iniciar = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 1)); // Verificar esse valor
+  };
 
-class App extends Component {
-    constructor() {
-        super();
-        this.deleteClock = this.deleteClock.bind(this);
-        this.addClock = this.addClock.bind(this);
+  var atualizaCs = time.cs, atualizaS = time.s, atualizaM = time.m, atualizaH = time.h;
+
+  const run = () => {
+    if(atualizaM === 60) {
+      atualizaH++;
+      atualizaM = 0;
     }
 
-    state = {
-        clocks: clocks,
-        next_id: num + 1,
+    if(atualizaS === 60) {
+      atualizaM++;
+      atualizaS = 0;
     }
 
-    deleteClock(id) {
-        const index = this.state.clocks.findIndex(clock => clock.id === id);
-        this.setState({
-            clocks: this.state.clocks.slice(0, index).concat(this.state.clocks.slice(index + 1))
-        });
+    if(atualizaCs === 100) { 
+      atualizaS++;
+      atualizaCs = 0;
     }
 
-    addClock() {
-        const name = prompt('Nome do relógio?');
-        const timezone = prompt('Fuso horário? https://momentjs.com/timezone/');
-        const clock = {
-            name: name,
-            timezone: timezone,
-            id: this.state.next_id
-        };
-        const nextClocks = [...this.state.clocks, clock];
-        this.setState({
-            next_id: this.state.next_id + 1,
-            clocks: nextClocks
-        });
-    }
-    
-    render() {
-        return (
-            <div className="App">
-                <h1>Relógio Mundial</h1>
-                {this.state.clocks.map(clock => {
-                    console.log(clock);
-                    return <Clock
-                        key={clock.id}
-                        {...clock}
-                        onDelete={this.deleteClock}
-                    />    
-                })}
-                <button className="Adicionar" onClick={(e) => this.addClock()}>Adicionar relógio</button>
+    atualizaCs++;
+    return setTime({cs:atualizaCs, s:atualizaS, m:atualizaM, h:atualizaH});
+  };
+
+  const parar = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const zerar = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({cs:0, s:0, m:0, h:0});
+  };
+
+  const retomar = () => iniciar();
+
+  const tempoParcial = () => { 
+    setStatus(3);
+    let time = this.state.H+ ":"+ this.state.M+ ":"+ this.state.S+ ":"+ this.state.Cs+ "\n\n"
+    setParcial([...parcial, time]);
+  };
+
+  tempoParcial();
+
+  return (
+    <div className="App-header">
+        <div className="clock-holder">
+            <div className="cronometro">
+              <DisplayComponent time={time}/>
+              <BtnComponent status={status} retomar={retomar} zerar={zerar} parar={parar} iniciar={iniciar}/>
             </div>
-        );
-    }
+        </div>
+    </div>
+  );
 }
 
 export default App;
